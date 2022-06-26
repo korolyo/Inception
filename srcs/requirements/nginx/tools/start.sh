cf7befc030a3720d565f307ec3222f583492af1a
Acollin <korolyo@gmail.com>
2022-06-26 20:51:13 +0300

    added -subj to start.sh in nginx

--- srcs/requirements/nginx/tools/start.sh
+++ srcs/requirements/nginx/tools/start.sh
@@ -1,4 +1,5 @@
 openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/nginx-selfsigned.key
-        -out /etc/ssl/certs/nginx-selfsigned.crt;
+        -out /etc/ssl/certs/nginx-selfsigned.crt \
+        -subj "/C=RU/ST=Moscow/L=21school/O=Acollin/OU=Org/CN=acollin.42.fr";
 
 nginx -g 'daemon off;'
