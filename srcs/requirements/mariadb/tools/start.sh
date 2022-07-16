7cfe7bd1f4f919913243e778d4bf8774e5356e41
Acollin <korolyo@gmail.com>
2022-07-16 17:23:27 +0300

    Processing..

--- srcs/requirements/mariadb/tools/start.sh
+++ srcs/requirements/mariadb/tools/start.sh
@@ -17,23 +17,22 @@ if [ ! -d "/var/lib/mysql/mysql" ]; then
 		return 1
 	fi
 
-	# https://stackoverflow.com/questions/10299148/mysql-error-1045-28000-access-denied-for-user-billlocalhost-using-passw
 	cat << EOF > $tfile
 USE mysql;
 FLUSH PRIVILEGES;
-DELETE FROM	mysql.user WHERE User='';
-DROP DATABASE test;
-DELETE FROM mysql.db WHERE Db='test';
-DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
-CREATE DATABASE $WP_DB_NAME CHARACTER SET utf8 COLLATE utf8_general_ci;
+CREATE DATABASE $WP_DB_NAME;
 CREATE USER '$WP_DB_USR'@'%' IDENTIFIED by '$WP_DB_PWD';
 GRANT ALL PRIVILEGES ON $WP_DB_NAME.* TO '$WP_DB_USR'@'%';
-FLUSH PRIVILEGES;
 ALTER USER 'root'@'localhost' IDENTIFIED BY '$WP_DB_ROOT_PWD';
+FLUSH PRIVILEGES;
 EOF
 	# run init.sql
 	/usr/bin/mysqld --user=mysql --bootstrap < $tfile
 	rm -f $tfile
 fi
 
-exec /usr/bin/mysqld --user=mysql --console
+# allow remote connections
+sed -i "s|skip-networking|# skip-networking|g" /etc/my.cnf.d/mariadb-server.cnf
+sed -i "s|.*bind-address\s*=.*|bind-address=0.0.0.0|g" /etc/my.cnf.d/mariadb-server.cnf
+
+exec /usr/bin/mysqld --user=mysql --console --skip-name-resolve --skip-networking=0 $@
